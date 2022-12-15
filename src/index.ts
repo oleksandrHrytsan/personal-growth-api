738a08fdaa5e911ac2fd771e506a42d6a605f65f
Oleksandr Hrytsan <oleksandrhrytsan27@gmail.com>
2022-12-15 11:12:44 +0200

    make user class constructor

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,7 @@ import { User } from "./entity/User"
 AppDataSource.initialize().then(async () => {
 
     console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
+    const user = new User('John', 'Doe', 35)
     await AppDataSource.manager.save(user)
     console.log("Saved a new user with id: " + user.id)
 
